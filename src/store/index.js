828a24287aed4740e9b0413a7b61410fb954c4bc
rafidaky <rafidakn@gmail.com>
2024-06-28 15:56:40 +0200

    cars project final version

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  carsReducer,
+  addCar,
+  removeCar,
+  changeSearchTerm,
+} from "./slices/carsSlice";
+import { changeCost, changeName, formReducer } from "./slices/formSlice";
+
+const store = configureStore({
+  reducer: {
+    cars: carsReducer,
+    form: formReducer,
+  },
+});
+
+export { store, changeCost, changeName, addCar, removeCar, changeSearchTerm };
---
+++ src/store/slices/carsSlice.js
@@ -0,0 +1,27 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const carsSlice = createSlice({
+  name: "cars",
+  initialState: {
+    searchTerm: "",
+    data: [],
+  },
+  reducers: {
+    changeSearchTerm(state, action) {
+      state.searchTerm = action.payload;
+    },
+    addCar(state, action) {
+      state.data.push({
+        name: action.payload.name,
+        cost: action.payload.cost,
+        id: nanoid(),
+      });
+    },
+    removeCar(state, action) {
+      state.data = state.data.filter((car) => car.id !== action.payload);
+    },
+  },
+});
+
+export const { changeSearchTerm, addCar, removeCar } = carsSlice.actions;
+export const carsReducer = carsSlice.reducer;
